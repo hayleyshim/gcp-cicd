abecb85ea59f9625528e0445e25e71e86906031e
hayleyshim <yhshim17@gmail.com>
2022-04-02 15:45:38 +0900

    Revert "cicd test"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "cicd test!!!!!!!!!!!!!!!"
+    return "cicd test"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
